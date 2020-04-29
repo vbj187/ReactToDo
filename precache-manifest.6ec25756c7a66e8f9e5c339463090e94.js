87e6d05d9afaa05f7d472fc5ee66dfa9f1331e16
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-04-29 23:46:00 +0530

    Updates

---
+++ precache-manifest.6ec25756c7a66e8f9e5c339463090e94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f5ff28f74a43fa1c3c62af167289e8",
+    "url": "/ReactToDo/index.html"
+  },
+  {
+    "revision": "0600e45b3ce5fbd6cc78",
+    "url": "/ReactToDo/static/js/2.c789eac3.chunk.js"
+  },
+  {
+    "revision": "e48cbd3f14fd18846c05",
+    "url": "/ReactToDo/static/js/main.7914b256.chunk.js"
+  },
+  {
+    "revision": "8e5c347663c0393c996c",
+    "url": "/ReactToDo/static/js/runtime-main.4938bf21.js"
+  }
+]);
